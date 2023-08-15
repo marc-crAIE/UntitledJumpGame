e3c7df897b4473f134d669e6b7096f38e869dc9f
Jay Pounsett <83759809+JayPounsett@users.noreply.github.com>
2023-08-15 18:52:36 +0930

    Keyboard Controls Added
    - Created ground plane and cube (with colours) to test controls.
    - Created PlayerMovement.cs with ability to move left/right (A/D) and jump (Space).
    - Added Rigidbody, Player Input and the PlayerMovement script to Player/Cube.
    - Created folder for Input Systems (InputActions & InputSystem)

---
+++ UntitledJumpGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Serialized Fields
+        [SerializeField] private float moveSpeed = 5.0f;
+        [SerializeField] private float jumpForce = 6.0f;
+    #endregion SerializeFields
+    #region Variables
+        private Rigidbody _myRigidbody;
+        //private BoxCollider _myCollider;
+        private Vector2 _inputVector;
+        /*
+        // Variables for stopping air-jumping <- No function created yet to utilise these
+        private bool _groundedPlayer;
+        private bool _jumpPressed = false;
+        */
+    #endregion Variables
+    
+    private void Awake()
+    {
+        _myRigidbody = GetComponent<Rigidbody>();
+        //_myCollider = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+#region Move
+    private void Move()
+    {
+        Vector3 movementDelta = _inputVector * moveSpeed * Time.deltaTime;
+        transform.position += movementDelta;
+    }
+    private void OnMove(InputValue value)
+    {
+        _inputVector = value.Get<Vector2>();
+    }
+#endregion Move
+
+#region Jump
+    private void OnJump()
+    {
+        // Need to add a check to see if touching the layer 'ground' to stop air jumping
+        _myRigidbody.velocity = new Vector2(_myRigidbody.velocity.x, jumpForce);
+    }
+#endregion Jump
+    
+}
