190f742c53fa67fb79e67dd2aef40016192c6b96
Jay Pounsett <83759809+JayPounsett@users.noreply.github.com>
2023-08-29 15:04:08 +0930

    Added Tilt Function
    - Moved GetComponent from Awake() to Start()
    - Added Tilt() function
    - Added tiltMoevSpeed serializefield

--- UntitledJumpGame/Assets/Scripts/PlayerMovement.cs
+++ UntitledJumpGame/Assets/Scripts/PlayerMovement.cs
@@ -10,13 +10,15 @@ public class PlayerMovement : MonoBehaviour
 {
     #region Serialized Fields
         [SerializeField] private float moveSpeed = 5.0f;
+        [SerializeField] private float tiltMoveSpeed = 10.0f;
     #endregion SerializeFields
     #region Variables
-        private Rigidbody rb;
-        private Vector2 inputVector;
+        Rigidbody rb;
+        Vector2 inputVector;
+        private float directionX;
     #endregion Variables
     
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
@@ -24,13 +26,31 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Move();
+        Tilt();
     }
- #region Move
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(directionX, rb.velocity.y);
+    }
+
+    #region Move
     private void Move()
     {
         Vector3 movementDelta = new Vector3(inputVector.x * moveSpeed * Time.deltaTime, inputVector.y * Time.deltaTime, 0f);
         transform.position += movementDelta;
     }
+
+    private void Tilt()
+    {
+        directionX = Input.acceleration.x * tiltMoveSpeed;
+        
+        var position = transform.position;
+        position = new Vector2(Mathf.Clamp(position.x, -25f, 25f), position.y);
+        
+        transform.position = position;
+    }
+    
     private void OnMove(InputValue value)
     {
         inputVector = value.Get<Vector2>();
