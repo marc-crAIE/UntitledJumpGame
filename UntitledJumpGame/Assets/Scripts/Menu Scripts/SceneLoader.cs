a0c759dd82272b76ea5033b8cb343d77081daf54
BrockBrierley <brock_brierley@outlook.com>
2023-09-21 09:44:45 +1000

    Menu now loads scenes and quits the game

---
+++ UntitledJumpGame/Assets/Scripts/Menu Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the next scene using an int as a reference
+    /// </summary>
+    /// <param name="sceneNumber">int referencing the build number of the scene</param>
+    public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    /// <summary>
+    /// Loads the next scene using a String as a reference
+    /// </summary>
+    /// <param name="SceneName">string containing the scenes name</param>
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
